958b44652198a2271e0534ba5b21bb8994267df2
yanaaaacl <a9506862137@yandex.ru>
2022-02-05 12:48:16 +0300

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+person3 = {"Name": "Ford Prefect",
+            "Gender": "Male",
+            "Occupation": "Researcher",
+            "Home Planet": "Betelgeuse Seven"}
+person3["Age"] = 33
+print(person3)
