a702cd1b87fe558cdde5e002909444edd5e5c35f
yanaaaacl <a9506862137@yandex.ru>
2022-02-10 10:13:34 +0300

    l

--- words1.py
+++ words1.py
@@ -1,9 +1,4 @@
-vowels = ["a", "e", "i", "o", "u"]
-word = "kirochka"
-found = []
-for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found.append(letter)
-for vowel in found:
-    print(vowel)
+vowels = {"a", "e", "i", "o", "u"}
+word = "hello"
+i = vowels.intersection(set(word))
+print(i)
