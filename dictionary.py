0249be7fcb28c2a26fde9bd85518b6fb45d418a5
yanaaaacl <a9506862137@yandex.ru>
2022-02-10 12:26:59 +0300

    d

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,19 @@
-person3 = {"Name": "Ford Prefect",
+import pprint
+people = {}
+people["Ford"] = {"Name": "Ford Prefect",
             "Gender": "Male",
             "Occupation": "Researcher",
             "Home Planet": "Betelgeuse Seven"}
-person3["Age"] = 33
-print(person3)
+people["Arthur"] = {"Name": "Arthur Dent", 
+                    "Gender": "Male", 
+                    "Occupation": "Sandwich-Marker",
+                    "Home Planet": "Earth"}
+people["Trillian"] = {"Name": "Tricia McMillan",
+                      "Gender": "Female",
+                      "Occupation": "Mathematician",
+                      "Home Planet": "Earth"}
+people["Robot"] = {"Name": "Marvin",
+                   "Gender": "Unknown",
+                   "Occupation": "Paranoid Android",
+                   "Home Planet": "Unknown"}
+print(pprint.pprint(people))
