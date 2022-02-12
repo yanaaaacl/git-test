2cf807df3ea4d5d4cbf3748efca9393b91852969
yanaaaacl <a9506862137@yandex.ru>
2022-02-12 12:14:40 +0300

    l

--- function.py
+++ function.py
@@ -1,5 +1,17 @@
-def multiply(a, b):
-    c = a * b
-    return c
-d, x = 3, 2
-print(multiply(d, x))
+l = list()
+print(l)
+l = [1, 2, 3]
+print(l)
+print()
+d = dict()
+print(d)
+d = {"first": 1, "second": 2, "third": 3}
+print(d)
+print()
+s = set()
+print(s)
+s = {1, 2, 3}
+print(s)
+print()
+t = (1, 2, 3)
+print(t)
