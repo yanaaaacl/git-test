fa5e81bb5aca7f2f83e7506c1a1bb821dd983a17
yanaaaacl <a9506862137@yandex.ru>
2022-02-12 10:49:14 +0300

    j

--- words1.py
+++ words1.py
@@ -1,4 +1,10 @@
-vowels = {"a", "e", "i", "o", "u"}
-word = "hello"
-i = vowels.intersection(set(word))
-print(i)
+def search4vowels(word):
+    # Выводит гласные, найденные во введенном слове.
+    vowels = {"a", "e", "i", "o", "u"}  
+    found = vowels.intersection(set(word))
+    for vowel in found:
+        print(vowel)
+a = "hello"
+search4vowels(a)
+
+
